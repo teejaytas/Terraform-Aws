db53d84d9ccec5e4fc83dd3723d159a5d66d712f
= <=>
2021-07-29 20:15:14 +0530

    update file

--- main.tf
+++ main.tf
@@ -1,124 +1,62 @@
 provider "aws" {
-  region     = "ap-south-1"
-  access_key = "AKIAZDX23APKHT3FLVKJ"
-  secret_key = "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"
+  region = "ap-south-1"
+}
+# Adding the Security Group
+
+resource "aws_security_group" "ec2-Security" {
+  name        = "ec2-security"
+
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 65535
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-security"
+  }
 }
 
-# resource "<provider>_<resource_type>" "name" {
-#     config options.....
-#     key = "value"
-#     key2 = "another value"
-    
-# }
-####create an aws instance#####
-resource "aws_instance" "first-server" {
+
+resource "aws_instance" "second-server" {
   ami  = "ami-0c1a7f89451184c8b"
   instance_type = "t2.micro"
-  key_name = "phase-one.pem"
+  key_name = "Pkey"
 
 
   user_data = <<-EOF
               #!/bin/bash
               sudo apt update -y
               sudo apt install nginx
+              sudo service nginx start
               EOF
     
     tags = {
-    Name = "ubuntu"
+    Name = "instance_name"
   }
 }
 
+##
 
-###first vpc#####
-resource "aws_vpc" "first-vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    name = "production"
-  }
-}
-###creating a subnet
-resource "aws_subnet" "subnet-1" {
-  vpc_id     = aws_vpc.first-vpc.id
-  cidr_block = "10.0.1.0/24"
-  tags = {
-    Name = "prod-subnet"
-  }
+resource "aws_key_pair" "key-pair" {
+  key_name   = "Pkey"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDK7ZjVr5fz6J1ioY/bh/4XHSP5vTxxI9pCFL28yOqT5fuxGtGh5tky+RhFF+u7iAoznNRost8ec8U6pvihU2n+Zsd45UcOF2V1UtZTvTFkXy0qBMgKBjE6WySoffSeH+PP4kHm2DdNL2bWAmifUe3Y3+M0seWMO+fidh5lVT20LIUvVopIXYiukxkGLDUl7qRfzZaMD/HIIIYeNKM0IlfAjor+x0Ll0oyGEhujNXkuF+KGUYvO81dzcQ3CsCQpZyg8J5s+FpjzHYKL6bIIytcIBAT/Yk01hhSJRXqZMN1UQsFafhGaO6lH4pImrAEv784aFv59u3FRaeZDalMML5KBSwE+uo/pJ/i3zdtjkp8hjIFXmQyEXAYWh0mFVM6qImafWDdxDUTcNhpQSFTERnXfbVf0FepwQOyXrx9FAS+AUnq/nnW6xaTlTLcMRTFxhOMBLP6zT6KcZAA87UwOhynfLYedlnQ31U4nK/yxBoIZ0ITWGA8RO9gq0aL6uYmFSuM= sigmoid@sv-lap-0090"
+
 }
 
-####second vpc######
-# resource "aws_vpc" "second-vpc" {
-#   cidr_block = "10.1.0.0/16"
-#   tags = {
-#     name = "Dev"
-#   }
-# }
-
-####resource "aws_subnet" "subnet-2" {
-#   vpc_id     = aws_vpc.second-vpc.id
-#   cidr_block = "10.1.1.0/24"
-#   tags = {
-#     Name = "dev-subnet"
-#   }
-# }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ###creating a vpc 
-# resource "aws_vpc" "prod-vpc" {
-#   cidr_block = "10.0.0.0/16"
-#   tags = {
-#       Name = "production"
-#     }
-# }
-
-
-# ###provinding a internet gateway
-# resource "aws_internet_gateway" "gw" {
-#   vpc_id = aws_vpc.prod-vpc.id
-
-# }
-
-# resource "aws_route_table" "prod-route-table" {
-#   vpc_id = aws_vpc.prod-vpc.id
-
-# #creating  a default route 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.gw.id
-#   }
-
-#   route {
-#     ipv6_cidr_block        = "::/0"
-#     egress_only_gateway_id = aws_internet_gateway.gw.id
-#     #our traffic from the subnet that's going to create can get out to the internet
-#   }
-
-#   tags = {
-#     Name = "prod"
-#   }
-# }
-
-
-
-# ###create a subnet 
-# resource "aws_subnet" "subnet-1" {
-#     vpc_id = aws_vpc.prod-vpc.id    
-#     cidr_block = "10.0.1.0/24"
-#     availability_zone = "ap-south-1"
-
-#     tags = {
-#       Name = "prod-subnet"
-#     }
-# }
+##
